da194544ddad379a851e0dcb56ba0ff8fa24767b
Abiel Deneke <abiel.deneke@gmail.com>
2025-04-10 00:14:59 -0500

    Create image output

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ int main(int argc, char *argv[])
     printf("Width: %i\n", width);
     printf("Height: %i\n", height);
 
+    img_save_pixels(argv[2], pixels, width, height, IMG_FMT_RGBA32);
+
     img_free_pixels(pixels);
 
     return 0;
