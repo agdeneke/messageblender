2f342e37981f9907df9387fa57abcc09486e799f
Abiel Deneke <abiel.deneke@gmail.com>
2025-04-11 02:46:37 -0500

    Implement Least Significant Bit insertion

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <imago2.h>
 #include <stdio.h>
+#include <string.h>
 
 #define MESSAGE_SIZE 256
 
@@ -14,6 +15,16 @@ int main(int argc, char *argv[])
 
     printf("Width: %i\n", width);
     printf("Height: %i\n", height);
+
+    int i, j;
+    for (i = 0; i < strlen(msg); i++) {
+        for (j = 7; j >= 0; j--) {
+            if ((msg[i] >> j & 1) == 1)
+                pixels[i * 8 + (7 - j)] |= 1;
+            else
+                pixels[i * 8 + (7 - j)] &= ~1;
+        }
+    }
 
     img_save_pixels(argv[2], pixels, width, height, IMG_FMT_RGBA32);
 
