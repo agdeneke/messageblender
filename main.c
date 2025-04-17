2dba3be4a116c641a09bfb5e559e7acf9157c642
Abiel Deneke <abiel.deneke@gmail.com>
2025-04-17 01:57:00 -0500

    Add message decoding

--- main.c
+++ main.c
@@ -18,23 +18,36 @@ void encode_message(char *msg, unsigned char *pixels)
     }
 }
 
+void decode_message(char *msg, unsigned char *pixels)
+{
+    int i, j;
+    for (i = 0; i < MESSAGE_SIZE; i++) {
+        msg[i] = 0;
+        for (j = 7; j >= 0; j--)
+            msg[i] |= (pixels[i * 8 + (7 - j)] & 1) << j;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     int width, height;
     char msg[MESSAGE_SIZE];
 
-    if (argc < 3) {
-        printf("Usage: messageblender <input file> <output file>\n");
+    if (argc < 2) {
+        printf("Usage: messageblender <input file> [-e output file]\n");
         exit(1);
     }
 
-    scanf("%s", msg);
-
     unsigned char *pixels = img_load_pixels(argv[1], &width, &height, IMG_FMT_RGBA32);
 
-    encode_message(msg, pixels);
-
-    img_save_pixels(argv[2], pixels, width, height, IMG_FMT_RGBA32);
+    if (argc >= 4 && !strcmp(argv[2], "-e")) {
+        scanf("%s", msg);
+        encode_message(msg, pixels);
+        img_save_pixels(argv[3], pixels, width, height, IMG_FMT_RGBA32);
+    } else {
+        decode_message(msg, pixels);
+        printf("Hidden Message: %s\n", msg);
+    }
 
     img_free_pixels(pixels);
 
