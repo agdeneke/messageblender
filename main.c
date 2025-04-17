673095477384a97a5448ce11ce04249609903069
Abiel Deneke <abiel.deneke@gmail.com>
2025-04-16 23:36:07 -0500

    Move message encoding to separate function

--- main.c
+++ main.c
@@ -5,6 +5,19 @@
 
 #define MESSAGE_SIZE 256
 
+void encode_message(char *msg, unsigned char *pixels)
+{
+    int i, j;
+    for (i = 0; i < strlen(msg); i++) {
+        for (j = 7; j >= 0; j--) {
+            if ((msg[i] >> j & 1) == 1)
+                pixels[i * 8 + (7 - j)] |= 1;
+            else
+                pixels[i * 8 + (7 - j)] &= ~1;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     int width, height;
@@ -19,15 +32,7 @@ int main(int argc, char *argv[])
 
     unsigned char *pixels = img_load_pixels(argv[1], &width, &height, IMG_FMT_RGBA32);
 
-    int i, j;
-    for (i = 0; i < strlen(msg); i++) {
-        for (j = 7; j >= 0; j--) {
-            if ((msg[i] >> j & 1) == 1)
-                pixels[i * 8 + (7 - j)] |= 1;
-            else
-                pixels[i * 8 + (7 - j)] &= ~1;
-        }
-    }
+    encode_message(msg, pixels);
 
     img_save_pixels(argv[2], pixels, width, height, IMG_FMT_RGBA32);
 
